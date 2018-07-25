fdd5fcc41fedf21cec49f90180cd4d0cad072730
Tony Robertson <tonyr729@gmail.com>
2018-07-24 22:53:01 -0600

    Modify codepen code

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,23 @@
 
 document.getElementById('btn__show-accordion').addEventListener("click", function(event) {
     document.getElementById('model').classList.remove('hidden')
-});
+    document.getElementById('accordion').classList.remove('hidden')
+  });
+  
+  document.getElementById('btn__show-flexcards').addEventListener("click", function(event) {
+    document.getElementById('model').classList.remove('hidden')
+    document.getElementById('flex-cards').classList.remove('hidden')
+  });
+  
+  document.getElementById('btn__show-headers').addEventListener("click", function(event) {
+    document.getElementById('model').classList.remove('hidden')
+    document.getElementById('headers').classList.remove('hidden')
+  });
+  
+  document.getElementById('model').addEventListener("click", function(event) {
+    document.getElementById('model').classList.add('hidden')
+    document.getElementById('accordion').classList.add('hidden')
+    document.getElementById('flex-cards').classList.add('hidden')
+    document.getElementById('headers').classList.add('hidden')
+  });
 
-document.getElementById('model').addEventListener("click", function(event) {
-  document.getElementById('model').classList.add('hidden')
-});
